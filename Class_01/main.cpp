126b3d8d130496240e36b817d11a4e932804c451
gunyoung0507-code <gunyoung0507@gmail.com>
2025-09-02 13:43:30 +0000

    혼자 해보는 첫 깃허브 활동

---
+++ Class_01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "첫날의 시범 수행" << endl;
+    int input;
+    cout << " 자연수를 입력해주세요." << endl;
+    cin >> input;
+    cout << "입력한 자연수는 " << input << "입니다." << endl;
+    return 0;
+}   
